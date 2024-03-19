58fa327ad90cf95521caca5dfb72c9414cd5b224
Anki <ankita38gupta38@gmail.com>
2024-03-19 11:35:23 +0530

    update

--- first.js
+++ first.js
@@ -121,23 +121,50 @@
 //  whrite a code which can give gardes to student according to their scores:
 // 90-100,A . 70-89,B. 60-69,C.50-59,D. 0-49,F
 
-let score=55;
-let grade;
-if (score>=90 && score<=100){
-    grade="A";
-}else if (score>= 70 && score<=89){
-    grade="B";
-}else if (score>=60 && score<=69){
-    grade="C";
+// let score=55;
+// let grade;
+// if (score>=90 && score<=100){
+//     grade="A";
+// }else if (score>= 70 && score<=89){
+//     grade="B";
+// }else if (score>=60 && score<=69){
+//     grade="C";
 
-}else if (score>= 50 && score<=59){
-    grade ="D"
-}else if (score>=0 && score<=49){
-    grade="F"
+// }else if (score>= 50 && score<=59){
+//     grade ="D"
+// }else if (score>=0 && score<=49){
+//     grade="F"
+// }
+// console.log("according to your scores, your grade was:",grade)
+
+
+// for loop 
+// for (count=1;count<=5; count++){
+//     console.log("hello world!")
+// }
+
+
+
+
+
+// for -of loop 
+// let str="Hello World!";
+// for (let i of str){
+//     console.log("i=",i)
+// }
+
+
+
+// for - in loop 
+let student={
+    name:"Ankita gupta ",
+    age:24,
+    cgpa:8.5,
+    ispass:true,
+};
+for (let i in student){
+    console.log(i);
 }
-console.log("according to your scores, your grade was:",grade)
-
-
 
 
 
