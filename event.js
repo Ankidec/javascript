31fcebc3d89913b080e7fda00c0a58d4d600df3d
Anki <ankita38gupta38@gmail.com>
2024-04-02 19:48:29 +0530

    event js

---
+++ event.js
@@ -0,0 +1,15 @@
+let modeBtn=document.querySelector("#mode");
+let currMode="light";//dark
+  
+
+modeBtn.addEventListener("click",()=>{
+    if (currMode ==="light"){
+        currMode="dark";
+        document.querySelector("body").style.backgroundColor="black";
+
+    }else{
+        currMode="light";
+        document.querySelector("body").style.backgroundColor="white";
+    }
+    console.log(currMode);
+});
